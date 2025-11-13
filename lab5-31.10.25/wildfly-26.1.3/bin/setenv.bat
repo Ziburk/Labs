af640cb198dc85c6997d0c75e129e37cf8cd17a1
Ziburk <ziburk76@gmail.com>
2025-11-13 15:27:53 +0300

    remove wildfly

--- lab5-31.10.25/wildfly-26.1.3/bin/setenv.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Example setenv.bat to set environment variables for WildFly startup on Windows
-set "UPLOAD_DIR=C:\wildfly-uploads"
-REM You can also set JVM options here, e.g.:
-REM set "JAVA_OPTS=%JAVA_OPTS% -Xms512M -Xmx1024M"
